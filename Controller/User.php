fca0f16fd275234fd97a40d4ceb90f3c611a8ee3
Eric <ericrakotomandimby@gmail.com>
2025-06-23 13:52:23 +0300

    js adjusted

--- Controller/User.php
+++ Controller/User.php
@@ -17,7 +17,7 @@ class User
         Load::template("sideAdmin");
         Load::view("user");
         Load::template("footer",[
-            "js"=>["reusable/jquery.min","reusable/sweetalert2.all.min","reusable/all","reusable/inser","reusable/selectimg","use"]
+            "js"=>["reusable/jquery.min","reusable/sweetalert2.all.min","reusable/all","reusable/insert","reusable/selectimg","user"]
         ]);
     }
 
