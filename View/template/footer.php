3a997789c380afef06f074daeb2a9e73d82675e6
Eric <ericrakotomandimby@gmail.com>
2025-06-23 13:43:22 +0300

    script footer changes

--- View/template/footer.php
+++ View/template/footer.php
@@ -3,38 +3,7 @@ if (isset($js)) {
     Load::js($js);
 }
 ?>
-<script>
-    const url = "http://localhost/logiciel/";
 
-    function insert(receiver, direction, formData) {
-        $.ajax({
-                url: url + receiver,
-                type: "post",
-                data: formData,
-                contentType: false,
-                processData: false
-            })
-            .done(function(res) {
-                if (res.status === "success") {
-                    alert("ok");
-                    window.location.href = url + direction;
-                    Swal.fire("Subscribed!", "Your file has been deleted.", "success");
-                }
-            })
-            .fail(function() {
-                alert("something went wrong");
-            });
-    }
-
-    $(document).ready(function() {
-        $(document.body).on("submit", "#formUser", function(prevent) {
-            prevent.preventDefault();
-            insert("User/insert", "", new FormData($(this)[0]))
-        })
-
-        selectImg("#photo");
-    })
-</script>
 </body>
 
 </html>
